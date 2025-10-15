80003cd229f388b16662baa7205faead2b269dbb
Eduardo Chico <eduardo.chico@gmail.com>
2025-10-14 20:32:50 -0600

    Delete frontend/main.tsx

--- frontend/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CommensalMock from "./App"; // renombra el TSX del canvas a App.tsx
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <CommensalMock />
-  </React.StrictMode>
-);
