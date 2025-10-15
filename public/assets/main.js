8808c31d0c75095ffac9e6682cf63872293f48a7
Eduardo Chico <eduardo.chico@gmail.com>
2025-10-14 20:17:21 -0600

    Delete public/assets/main.js

--- public/assets/main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CommensalMock from "/frontend/main.tsx"; // renombra el TSX del canvas a App.tsx
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <CommensalMock />
-  </React.StrictMode>
-);
